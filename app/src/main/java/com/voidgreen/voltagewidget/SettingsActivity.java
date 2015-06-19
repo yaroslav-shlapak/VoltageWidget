116e918b0ba439a7eecbed3424a01277cc76780a
Yaroslav <yarosalv.shlapak@gmail.com>
2015-06-19 09:24:22 +0300

    Widget configuration functionality was conveyed to SettingsActivity.saveWidgetModification()

--- app/src/main/java/com/voidgreen/voltagewidget/SettingsActivity.java
+++ app/src/main/java/com/voidgreen/voltagewidget/SettingsActivity.java
@@ -18,13 +18,19 @@ import android.widget.RemoteViews;
  */
 public class SettingsActivity extends PreferenceActivity {
     int mAppWidgetId;
-    public static final String EXTRA_STRING = "isFirstTime";
+    private static final String EXTRA_STRING = "isFirstTime";
     SharedPreferences sharedPreferences;
+    Context context;
+    RemoteViews views;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        context = getApplicationContext();
+        views = new RemoteViews(context.getPackageName(),
+                R.layout.widget_layout);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -33,10 +39,25 @@ public class SettingsActivity extends PreferenceActivity {
                     AppWidgetManager.EXTRA_APPWIDGET_ID,
                     AppWidgetManager.INVALID_APPWIDGET_ID);
         }
-        final Context context = getApplicationContext();
-        RemoteViews views = new RemoteViews(context.getPackageName(),
-                R.layout.widget_layout);
 
+
+
+        Intent i = new Intent(context, BatteryInfoService.class);
+        context.startService(i);
+        Log.d("SettingsActivity", "startService");
+
+/*            Intent intentSettings = new Intent(context, SettingsActivity.class);
+            intentSettings.putExtra(EXTRA_STRING, false);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentSettings, 0);
+            views.setOnClickPendingIntent(R.id.batteryInfoTextViewWidget, pendingIntent);*/
+
+    }
+
+    public void onApplyButtonClick(View v) {
+        saveWidgetModification();
+    }
+
+    private void saveWidgetModification() {
         VoltageWidgetData voltageWidgetData = new VoltageWidgetData(context);
         views.setTextColor(R.id.batteryInfoTextViewWidget, voltageWidgetData.getTextColor());
         views.setTextViewTextSize(R.id.batteryInfoTextViewWidget, TypedValue.COMPLEX_UNIT_SP, voltageWidgetData.getTextSize());
@@ -48,27 +69,9 @@ public class SettingsActivity extends PreferenceActivity {
         alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis() + 1000,
                 Long.parseLong(voltageWidgetData.getUpdateInterval()), pendingIntentAlarmManager);
 
-        Intent i = new Intent(context, BatteryInfoService.class);
-        context.startService(i);
-        Log.d("SettingsActivity", "startService");
-
-/*            Intent intentSettings = new Intent(context, SettingsActivity.class);
-            intentSettings.putExtra(EXTRA_STRING, false);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentSettings, 0);
-            views.setOnClickPendingIntent(R.id.batteryInfoTextViewWidget, pendingIntent);*/
-
-        Log.d("SettingsActivity", voltageWidgetData.getUpdateInterval());
-
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
         appWidgetManager.updateAppWidget(mAppWidgetId, views);
 
-    }
-
-    public void onApplyButtonClick(View v) {
-        saveWidgetModification();
-    }
-
-    private void saveWidgetModification() {
         Intent resultValue = new Intent();
         resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
         setResult(RESULT_OK, resultValue);
