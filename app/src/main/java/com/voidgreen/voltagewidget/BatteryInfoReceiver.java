9159202f5f2af0af1217b0003d51fa3e6136aa75
Yaroslav <yarosalv.shlapak@gmail.com>
2015-07-26 02:09:13 +0300

    Production release

--- app/src/main/java/com/voidgreen/voltagewidget/BatteryInfoReceiver.java
+++ app/src/main/java/com/voidgreen/voltagewidget/BatteryInfoReceiver.java
@@ -12,11 +12,15 @@ public class BatteryInfoReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 3000);
+
+        String string;
+
         if (voltage < 3000) {
-            voltage = voltage * 1000;
+            string = "Your device is not supported";
+        } else {
+            string = Integer.toString(voltage);
         }
 
-        String string = Integer.toString(voltage);
 
         Utility.saveBatteryInfo(context, string);
         Utility.updateAllWidgets(context);
